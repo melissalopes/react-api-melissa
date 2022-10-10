01f44661b2c7398a4a97c90713504e0dea68c391
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2022-10-10 16:38:30 -0300

    feat: renderizando arrays

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,26 +1,17 @@
 import React from "react";
 
 function Lista() {
+    const tasks = [{tarefa: "React", tempo: "02:00:00"}, {tarefa: "JavaScript", tempo: "01:00:00"}, {tarefa: "TypeScript", tempo: "01:00:00"}];
     return (
         <aside> 
             <h2>Estudos do dia</h2>
             <ul>
-                <li>
-                    <h3>
-                        React
-                    </h3>
-                    <span>
-                        02:00:00
-                    </span>
-                </li>
-                <li>
-                    <h3>
-                        Java Sript
-                    </h3>
-                    <span>
-                        01:00:00
-                    </span>
-                </li>
+                {tasks.map((task, index) => (
+                    <li key={index}>
+                        <h3>{task.tarefa}</h3>
+                        <span>{task.tempo}</span>
+                    </li>
+                ))}
             </ul>
 
         </aside>
