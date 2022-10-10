5521d9e61001dff0b5508bff6cdebfffff39ad7b
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2022-10-10 14:14:32 -0300

    feat: criando formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Formulario from './components/form';
 
 function App() {
   return (
     <div className="App">
+      <Formulario></Formulario>
     </div>
   );
 }
